7b46b410a37d7fd70f86dcc935b573c1420250f0
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-07-05 14:52:35 +0800

    Finish computerPlay() function allowing computer to choose between R/P/S Using random() function to generate a random choice

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 function random(range) {
     return Math.floor(range * Math.random());
 }
+
+// To generate the computer's actual choice depending on random()'s return value
+function computerPlay() {
+    const choiceNum = random(3);
+    switch (choiceNum) {
+        case 0:
+            return("Rock");
+            break;
+        case 1:
+            return("Paper");
+            break;
+        default:
+            return("Scissors");
+            break;
+    }
+}
